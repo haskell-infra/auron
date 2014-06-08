e87580cec2b496d343d39ed58079ed5904ac0939
Austin Seipp <aseipp@pobox.com>
2014-06-08 09:43:38 -0500

    vivi: Launch MariaDB 10 with spiped
    Summary:
    This adds the basic infrastructure to vivi for mysql01 - in particular, it just adds the newest MariaDB 10 package along with an spiped frontend controlled by systemd. There are maybe some tweaks we could make to the configuration, but this works just fine right now.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    
    Test Plan:
    Run 'nixops deploy --include mysql01' and watch it work. Jump in via SSH and run 'systemctl start spiped@mysql' to start the spiped frontend service.
    
    Reviewers: relrod
    
    Differential Revision: https://phabricator.haskell.org/D6

--- src/hosts/vivi.nix
+++ src/hosts/vivi.nix
@@ -9,5 +9,26 @@ with builtins;
 
 {
   require = [ ./common.nix ./duosec.nix ];
+
+  /* Networking configuration */
   networking.hostName = "vivi";
+  networking.firewall.allowedTCPPorts =
+    [ 9000 # spiped, no MariaDB
+    ];
+
+  /* MariaDB configuration */
+  services.mysql.enable  = true;
+  services.mysql.package = pkgs.mariadb;
+
+  /* Spiped frontend */
+  /* TODO: use a unix pipe for the target? */
+  services.spiped.enable = true;
+  services.spiped.config = {
+    mysql =
+      { keyfile = "/var/lib/spiped/mysql.key";
+        decrypt = true;
+        source  = "0.0.0.0:9000";
+        target  = "127.0.0.1:3306";
+      };
+  };
 }
