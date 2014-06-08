dd7831bf3cad428119c470a6536fd9d15fe91696
Austin Seipp <aseipp@pobox.com>
2014-06-08 11:21:18 -0500

    lulu: basic nginx configuration
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/hosts/lulu.nix
+++ src/hosts/lulu.nix
@@ -10,5 +10,64 @@ with builtins;
 
 {
   require = [ ./common.nix ./duosec.nix ];
+
+  /* Networking configuration */
   networking.hostName = "lulu";
+  networking.firewall.allowedTCPPorts =
+    [ 80 443
+    ];
+
+  /* Nginx configuration */
+  services.nginx.enable = true;
+  services.nginx.config = httpsOnly
+    { serverNames = "monitor.haskell.org";
+      hsts       = false;
+      xframeDeny = false;
+
+      upstreams = ''
+        upstream php { server unix:/var/run/php5-fpm.socket; }
+      '';
+
+      config = ''
+        root /usr/share/nginx/html/;
+
+        location /nagios {
+          auth_basic "Nagios Restricted Access (via nginx)";
+          auth_basic_user_file /etc/nagios/passwd;
+
+          alias /usr/share/nagios/html;
+          index index.php;
+        }
+
+        location ~ ^/nagios/(.*\.php)$ {
+          auth_basic "Nagios Restricted Access (via nginx)";
+          auth_basic_user_file /etc/nagios/passwd;
+
+          root /usr/share/nagios/html/;
+          rewrite ^/nagios/(.*) /$1 break;
+          fastcgi_index index.php;
+          include /etc/nginx/fastcgi_params;
+          fastcgi_param SCRIPT_FILENAME /usr/share/nagios/html$fastcgi_script_name;
+          fastcgi_pass unix:/var/run/php5-fpm.socket;
+        }
+
+        location ~ \.cgi$ {
+          auth_basic "Nagios Restricted Access (via nginx)";
+          auth_basic_user_file /etc/nagios/passwd;
+
+          root /usr/lib64/nagios/cgi-bin/;
+          rewrite ^/nagios/cgi-bin/(.*)\.cgi /$1.cgi break;
+          include /etc/nginx/fastcgi_params;
+          fastcgi_param AUTH_USER $remote_user;
+          fastcgi_param REMOTE_USER $remote_user;
+          fastcgi_param SCRIPT_FILENAME /usr/lib64/nagios/cgi-bin$fastcgi_script_name;
+          fastcgi_pass 127.0.0.1:8999;
+        }
+
+        location ~ \.php$ {
+          include /etc/nginx/fastcgi_params;
+          fastcgi_pass php;
+        }
+      '';
+    };
 }
