458a88f4dfa9441ed590c06790243b5496a5105b
Austin Seipp <aseipp@pobox.com>
2014-06-08 10:58:34 -0500

    cid: basic nginx configuration
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/hosts/cid.nix
+++ src/hosts/cid.nix
@@ -10,5 +10,21 @@ with builtins;
 
 {
   require = [ ./common.nix ./duosec.nix ];
+
+  /* Networking configuration */
   networking.hostName = "cid";
+  networking.firewall.allowedTCPPorts =
+    [ 80 443
+    ];
+
+  /* Nginx configuration */
+  services.nginx.enable = true;
+  services.nginx.config = httpPlusHttps
+    { serverNames = "haskell.org www.haskell.org";
+      config = ''
+        location / {
+          root /www/planet/public_html;
+        }
+      '';
+    };
 }
