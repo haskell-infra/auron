6d3310874cea204771f30ac03642fcd802464de5
Austin Seipp <aseipp@pobox.com>
2014-06-08 11:20:16 -0500

    cloud: basic nginx config
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/hosts/cloud.nix
+++ src/hosts/cloud.nix
@@ -9,5 +9,20 @@ with builtins;
 
 {
   require = [ ./common.nix ./duosec.nix ];
+
+  /* Networking configuration */
   networking.hostName = "cloud";
+  networking.firewall.allowedTCPPorts =
+    [ 80 443
+    ];
+
+  /* Nginx configuration */
+  services.nginx.enable = true;
+  services.nginx.config = httpPlusHttps
+    { serverNames = "git.haskell.org ghc.haskell.org";
+      config = ''
+        location / {
+        }
+      '';
+    };
 }
