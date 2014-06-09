9278bb7f5aa28e36a9bd20efa9e01c01821095dd
Austin Seipp <aseipp@pobox.com>
2014-06-09 13:32:21 -0500

    genspiped: Restart services too
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- bin/genspiped.sh
+++ bin/genspiped.sh
@@ -9,8 +9,9 @@ if [ -z "$1" ]; then
   fi
 fi
 
-dd bs=1 count=32 if=/dev/urandom of=temp.key
+dd bs=1 count=32 if=/dev/urandom of=temp.key > /dev/null 2>&1
 for x in $VMS; do
   nixops scp --to $x temp.key /var/lib/spiped/$KEYNAME.key
+  nixops ssh $x -- systemctl restart spiped@$KEYNAME > /dev/null 2>&1
 done
 rm -f temp.key
