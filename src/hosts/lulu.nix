89db2842fd8a4ecb81f517ddff46702ca2c34f73
Ricky Elrod <ricky@elrod.me>
2014-06-22 00:28:03 -0500

    lulu: Initial Nagios configuration
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/hosts/lulu.nix
+++ src/hosts/lulu.nix
@@ -9,8 +9,18 @@
 with builtins;
 with import ./roles/nginx.nix { inherit lib; };
 
+let
+  nagiosCGICfgFile = pkgs.writeText "nagios.cgi.conf"
+    ''
+      main_config_file=/etc/nagios.cfg
+      use_authentication=0
+      url_html_path=/nagios
+    '';
+in
 {
-  require = [ ./roles/common.nix ./roles/gencert.nix ];
+  require = [ ./roles/common.nix
+              ./roles/gencert.nix
+              ./roles/nagios.nix ];
 
   /* Networking configuration */
   networking.hostName = "lulu";
@@ -18,58 +28,85 @@ with import ./roles/nginx.nix { inherit lib; };
     [ 80 443
     ];
 
+  services.fcgiwrap.enable = true;
+  services.fcgiwrap.bindSocket = "tcp:127.0.0.1:8999";
+
   /* Nginx configuration */
   services.nginx.enable = true;
   services.nginx.config = httpsOnly
-    { serverNames = "monitor.haskell.org";
+    { serverNames = "monitor.haskell.org monitor";
       hsts       = false;
       xframeDeny = false;
 
-      upstreams = ''
-        upstream php { server unix:/var/run/php5-fpm.socket; }
-      '';
-
       config = ''
-        root /usr/share/nginx/html/;
+        root ${pkgs.nagios}/share;
 
         location /nagios {
-          auth_basic "Nagios Restricted Access (via nginx)";
-          auth_basic_user_file /etc/nagios/passwd;
+          #auth_basic "Nagios Restricted Access (via nginx)";
+          #auth_basic_user_file /etc/nagios/passwd;
 
-          alias /usr/share/nagios/html;
+          alias ${pkgs.nagios}/share;
           index index.php;
         }
 
         location ~ ^/nagios/(.*\.php)$ {
-          auth_basic "Nagios Restricted Access (via nginx)";
-          auth_basic_user_file /etc/nagios/passwd;
+          #auth_basic "Nagios Restricted Access (via nginx)";
+          #auth_basic_user_file /etc/nagios/passwd;
 
-          root /usr/share/nagios/html/;
+          root ${pkgs.nagios}/share;
           rewrite ^/nagios/(.*) /$1 break;
           fastcgi_index index.php;
-          include /etc/nginx/fastcgi_params;
-          fastcgi_param SCRIPT_FILENAME
-            /usr/share/nagios/html$fastcgi_script_name;
-          fastcgi_pass unix:/var/run/php5-fpm.socket;
+          #required if PHP was built with --enable-force-cgi-redirect
+          fastcgi_param  REDIRECT_STATUS    200;
+          #variables to make the $_SERVER populate in PHP
+          fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;
+          fastcgi_param  QUERY_STRING       $query_string;
+          fastcgi_param  REQUEST_METHOD     $request_method;
+          fastcgi_param  CONTENT_TYPE       $content_type;
+          fastcgi_param  CONTENT_LENGTH     $content_length;
+          fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
+          fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+          fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;
+          fastcgi_param  REMOTE_ADDR        $remote_addr;
+          fastcgi_pass   unix:/run/phpfpm/nagios.sock;
         }
 
         location ~ \.cgi$ {
-          auth_basic "Nagios Restricted Access (via nginx)";
-          auth_basic_user_file /etc/nagios/passwd;
+          #auth_basic "Nagios Restricted Access (via nginx)";
+          #auth_basic_user_file /etc/nagios/passwd;
 
-          root /usr/lib64/nagios/cgi-bin/;
+          root ${pkgs.nagios}/sbin;
           rewrite ^/nagios/cgi-bin/(.*)\.cgi /$1.cgi break;
-          include /etc/nginx/fastcgi_params;
-          fastcgi_param AUTH_USER $remote_user;
-          fastcgi_param REMOTE_USER $remote_user;
-          fastcgi_param SCRIPT_FILENAME
-            /usr/lib64/nagios/cgi-bin$fastcgi_script_name;
+          #required if PHP was built with --enable-force-cgi-redirect
+          fastcgi_param  REDIRECT_STATUS    200;
+          fastcgi_param  NAGIOS_CGI_CONFIG ${nagiosCGICfgFile};
+          #variables to make the $_SERVER populate in PHP
+          fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;
+          fastcgi_param  QUERY_STRING       $query_string;
+          fastcgi_param  REQUEST_METHOD     $request_method;
+          fastcgi_param  CONTENT_TYPE       $content_type;
+          fastcgi_param  CONTENT_LENGTH     $content_length;
+          fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
+          fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+          fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;
+          fastcgi_param  REMOTE_ADDR        $remote_addr;
           fastcgi_pass 127.0.0.1:8999;
         }
 
         location ~ \.php$ {
-          include /etc/nginx/fastcgi_params;
-          fastcgi_pass php;
+          #required if PHP was built with --enable-force-cgi-redirect
+          fastcgi_param  REDIRECT_STATUS    200;
+          #variables to make the $_SERVER populate in PHP
+          fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;
+          fastcgi_param  QUERY_STRING       $query_string;
+          fastcgi_param  REQUEST_METHOD     $request_method;
+          fastcgi_param  CONTENT_TYPE       $content_type;
+          fastcgi_param  CONTENT_LENGTH     $content_length;
+          fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
+          fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+          fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;
+          fastcgi_param  REMOTE_ADDR        $remote_addr;
+          fastcgi_pass   unix:/run/phpfpm/nagios.sock;
         }
       '';
     };
--- src/hosts/roles/nagios.nix
+++ src/hosts/roles/nagios.nix
@@ -1,3 +1,69 @@
-{
-# TODO FIXME
+{ config, pkgs, resources, lib, ... }:
+
+let
+  php = pkgs.php54;
+
+  hostgroupsCfg = pkgs.writeText "hostgroups.cfg" ''
+    define hostgroup {
+      hostgroup_name ssh
+      alias          servers with ssh
+      members        *
+    }
+  '';
+
+  servicesCfg = pkgs.writeText "services.cfg" ''
+    define service {
+      hostgroup ssh
+      service_description SSH
+      check_command check_ssh
+      use generic-service
+    }
+  '';
+
+  hostsCfg = pkgs.writeText "hosts.cfg" ''
+    define host {
+      use linux-server
+      host_name new-www.haskell.org
+      alias www.haskell.org
+      address 88.198.224.243
+    }
+  '';
+
+  contactsCfg = pkgs.writeText "contacts.cfg" ''
+    define contact {
+      contact_name relrod
+      use generic-contact
+      alias Ricky Elrod
+      email ricky.haskell@elrod.me
+    }
+  '';
+
+in {
+  services.nagios.enable = true;
+  services.nagios.enableWebInterface = true;
+  services.nagios.objectDefs = [
+    "${pkgs.nagios}/etc/objects/commands.cfg"
+    "${pkgs.nagios}/etc/objects/contacts.cfg"
+    "${pkgs.nagios}/etc/objects/templates.cfg"
+    "${pkgs.nagios}/etc/objects/timeperiods.cfg"
+    hostgroupsCfg
+    servicesCfg
+    hostsCfg
+    contactsCfg
+  ];
+
+  #systemd.services.phpfpm.environment = { PHPRC = phpIni; };
+  services.phpfpm.phpPackage = php;
+  services.phpfpm.poolConfigs =
+    { nagios = ''
+        listen = /run/phpfpm/nagios.sock
+        user = nagios
+        pm = dynamic
+        pm.max_children = 75
+        pm.start_servers = 10
+        pm.min_spare_servers = 5
+        pm.max_spare_servers = 20
+        pm.max_requests = 500
+      '';
+    };
 }
